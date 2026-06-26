3c08ee14aaf1a9e1be80e8651d96f84eabc931d4
wihlner-addcream <mattias.w@addcream.se>
2026-06-26 22:53:03 +0200

    current

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://boringworks.se",
+  site: "https://www.boringworks.se",
   vite: {
     plugins: [tailwindcss()],
   },
